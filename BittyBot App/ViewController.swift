a3ae2da995d128c8442d943af36297dde3f58c9b
bilalzahid26 <bilal.zahid99@yahoo.com>
2018-02-05 19:03:43 +0000

    hello

--- BittyBot App/ViewController.swift
+++ BittyBot App/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var labelName: UILabel!
+    @IBAction func buttonClick(sender: UIButton) {
+        self.getHTML()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getHTML(){
+        let myURLString = "https://coinmarketcap.com"
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return
+        }
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            var HTMLArr = myHTMLString.components(separatedBy:"<tr id")
+            var tokens = [token]()
+            HTMLArr[0] = ""
+            let word = HTMLArr[HTMLArr.count-1]
+            if let index = word.range(of:"></a></td>")?.lowerBound {
+                let substring = word[..<index]
+                let string = String(substring)
+                HTMLArr[HTMLArr.count-1] = String(string)
+            }
+            var i = 0
+            for coin in HTMLArr{
+                if coin != "" {
+                tokens.append(token(tokenCode: coin))
+                }
+                i = i + 1
+            }
+            
+        } catch let error {
+            labelName.text = "Error: \(error)"
+        }
     }
+}
 
+class token {
+    var tokenName: String
+    init(tokenCode: String) {
 
+        tokenName = String(tokenCode.prefix(30))
+    }
+    func getTokenName() -> String {
+        return tokenName
+    }
 }
 
